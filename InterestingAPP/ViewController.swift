32afcba03c7af24e78aae4fe3ac374155314ecb9
YongShunTu <a49980060@gmail.com>
2021-11-30 23:05:44 +0800

    Initial Commit

---
+++ InterestingAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InterestingAPP
+//
+//  Created by 姜又寧 on 2021/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
